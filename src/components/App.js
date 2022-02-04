107d7217a26dae473b279d068e19279fdc2ad545
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-04 08:09:06 +0200

    basic structure
    https://codepen.io/Coding-Artist/pen/bGRgqbg?editors=1010

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
+import Result from './Result';
 import './style/App.css';
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div class="wrapper">
+      <audio id="sound"></audio>
+      <div class="container">
+        <div class="search-box">
+          <input
+            type="text"
+            placeholder="Type the word here..."
+            id="inp-word"
+          />
+
+          <button id="search-btn">Search</button>
+        </div>
+        <Result />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Result.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Result() {
+  const word = 'hello';
+  const partOfSpeech = 'n.';
+  const definition = 'greating word in english also mean peace';
+  return (
+    <div id="result" class="result">
+      <div class="word">
+        <h3>{word}</h3>
+        <button onclick="playSound()">
+          {' '}
+          <i class="fas fa-volume-up"></i>
+        </button>
+      </div>
+      <div class="details">
+        <p>{partOfSpeech}</p>
+      </div>
+      <p class="word-meaning">{definition}</p>
+    </div>
+  );
+}
