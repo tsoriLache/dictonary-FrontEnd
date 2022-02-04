b2f6197a2c37c148f891615ec51597f672b81b31
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-04 09:41:20 +0200

    class->className

--- src/components/ChoosePart.js
+++ src/components/ChoosePart.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './style/ChoosePart.scss';
 export default function ChoosePart() {
   return (
-    <div class="button-group">
-      <button class="e-mg-b--md bg--blue-grad">Button</button>
-      <button class="e-mg-b--md bg--green-grad">Button</button>
-      <button class="e-mg-b--md bg--red-grad">Button</button>
-      <button class="e-mg-b--md bg--purple-grad">Button</button>
+    <div className="button-group">
+      <button className="e-mg-b--md bg--blue-grad">Button</button>
+      <button className="e-mg-b--md bg--green-grad">Button</button>
+      <button className="e-mg-b--md bg--red-grad">Button</button>
+      <button className="e-mg-b--md bg--purple-grad">Button</button>
     </div>
   );
 }
