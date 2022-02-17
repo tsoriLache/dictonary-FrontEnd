7696e370974995accb91f1352bda0dd4f124ec18
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-17 13:58:18 +0200

    react-router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Random from './components/Random';
+import ErrorPage from './components/ErrorPage';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="part-of-speech" element={<Random />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
