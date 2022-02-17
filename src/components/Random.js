16ed50f69e95d53f03f2d66f68673f45f4b693e2
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-17 14:00:10 +0200

    react-select

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,3 +1,5 @@
+import Select from 'react-select';
+import './style/Random.css';
 const notyf = new Notyf({
   duration: 1000,
   position: {
@@ -15,4 +17,41 @@ const notyf = new Notyf({
 });
 
 // Display an error notification
+
+const options = [
+  { value: 'a', label: 'A' },
+  { value: 'b', label: 'B' },
+  { value: 'c', label: 'C' },
+  { value: 'd', label: 'D' },
+  { value: 'e', label: 'E' },
+  { value: 'f', label: 'F' },
+  { value: 'g', label: 'G' },
+  { value: 'h', label: 'H' },
+  { value: 'i', label: 'I' },
+  { value: 'j', label: 'J' },
+  { value: 'k', label: 'K' },
+  { value: 'l', label: 'L' },
+  { value: 'm', label: 'M' },
+  { value: 'n', label: 'N' },
+  { value: 'o', label: 'O' },
+  { value: 'p', label: 'P' },
+  { value: 'q', label: 'Q' },
+  { value: 'r', label: 'R' },
+  { value: 's', label: 'S' },
+  { value: 't', label: 'T' },
+  { value: 'u', label: 'U' },
+  { value: 'v', label: 'V' },
+  { value: 'w', label: 'W' },
+  { value: 'x', label: 'X' },
+  { value: 'y', label: 'Y' },
+  { value: 'z', label: 'Z' },
+];
+
+  const [selectedOption, setSelectedOption] = useState({ value: '' });
     result.data.err ? notyf.error('Not Found') : setData(result.data);
+          <Select
+            className="selector"
+            defaultValue={selectedOption}
+            onChange={setSelectedOption}
+            options={options}
+          />
