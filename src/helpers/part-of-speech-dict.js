4866ffc37a04a004ca95a4db385870a3dc633222
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-17 14:02:21 +0200

    Create part-of-speech-dict.js
    helper dict

---
+++ src/helpers/part-of-speech-dict.js
@@ -0,0 +1,11 @@
+const posDict = {
+  noun: 'n',
+  pronoun: 'pron',
+  verb: 'v',
+  adjective: 'a',
+  adverb: 'adv',
+  preposition: 'prep',
+  conjunction: 'conj',
+  interjection: 'interj',
+};
+export default posDict;
